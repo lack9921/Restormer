684cad2194b157497bfebcb060588eabc81f806a
Elvis <103756294+lack9921@users.noreply.github.com>
2026-05-29 18:25:18 +0000

    Refactor options handling, update version, and enhance demo script
    - Refactored `options.py` for improved readability and maintainability.
    - Updated version information in `version.py` to reflect recent changes.
    - Enhanced `demo.py` to support additional tasks and improved image handling.
    - Modified `setup.cfg` and `setup.py` for better configuration management.
    - Updated `train.sh` to ensure compatibility with distributed training.

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Mar  9 22:05:30 2022
-__version__ = '1.2.0+10018c6'
+# TIME: Fri May 29 18:10:03 2026
+__version__ = '1.2.0+2b272eb'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
